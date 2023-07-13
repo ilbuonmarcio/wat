86fe17367ba8b244d9c2d9f491708cf7f94573de
ilbuonmarcio <marciozgaming@gmail.com>
2023-07-13 02:49:59 +0200

    Added graphing

--- README.md
+++ README.md
@@ -18,6 +18,15 @@ I do it via a cron command like the following:
 * * * * * python /srv/services/wat/main.py
 ```
 
+After some time, check results with the `show.py` script!
+
+```bash
+python /srv/services/wat/show.py
+```
+
+Output:
+![Input/Output](input_output.png)
+
 ### License
 
 WTFPL License, see `LICENSE` file for more information.
---
+++ show.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pymysql.cursors
+from datetime import datetime, timedelta
+    
+datetime_begin = datetime.today() - timedelta(days=1)
+
+connection = pymysql.connect(host='localhost',
+                             user='wat',
+                             password='123',
+                             database='wat',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+# Getting records from database
+with connection:
+    with connection.cursor() as cursor:
+        # Create a new record
+        sql = """SELECT
+                    HOUR(datetimestamp) AS hour,
+                    MIN(input) AS min_input,
+                    MAX(input) AS max_input,
+                    MIN(output) AS min_output,
+                    MAX(output) AS max_output
+                FROM
+                    logs
+                WHERE
+                    datetimestamp >= %s
+                GROUP BY
+                    HOUR(datetimestamp)
+                ORDER BY
+                    datetimestamp ASC;"""
+        cursor.execute(sql, (datetime_begin))
+
+        result = cursor.fetchall()
+        result = [[str(row['hour']) for row in result], [row['max_input'] for row in result], [row['max_output'] for row in result], [row['min_input'] for row in result], [row['min_output'] for row in result]]
+
+        # f, ax = plt.subplots()
+        # ax.plot([*set(result[0])])
+        # ymin, ymax = ax.get_xlim()
+        # ax.set_yticks(np.round(np.linspace(ymin, ymax, len(result[1]) // 2), 2))
+
+        plt.plot(result[0], result[1])
+        plt.plot(result[0], result[2])
+        plt.plot(result[0], result[3])
+        plt.plot(result[0], result[4])
+        plt.ylabel('Input/Output')
+        plt.savefig('input_output.png', dpi=300)
